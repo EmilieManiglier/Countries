8f0c8ea93c12f079006961b73d5cd74f1bd8917e
Emilie-Excalibur <emiliemaniglier@gmail.com>
2020-06-12 22:40:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Countries
+
+Learn the locations of countries all around the world with this app !
+
+Framework JS : React
+API : REST countries
---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import
+import React from 'react';
+
+import './styles.scss';
+
+// == Component
+const App = () => (
+  <div className="app">
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default App;
